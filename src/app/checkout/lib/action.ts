39f583519f70fbc34808bf48c7f203deeafcdec3
Tobias Maier <tobias.maier@republik.ch>
2024-06-14 14:46:29 +0200

    feat: init shadcn ui

--- src/app/checkout/lib/action.ts
+++ src/app/checkout/lib/action.ts
@@ -9,20 +9,21 @@ type MonthlyAboPurchase = {
 export async function initMonthlyAboPurchase(): Promise<MonthlyAboPurchase> {
   const stripe = initStripe("REPUBLIK");
 
+  const monthlyAboProduct = await stripe.products.retrieve(monthlyAboApiID);
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
+          currency: "chf",
+          product: monthlyAboProduct.id,
+          unit_amount: monthlyAboProduct.,
         },
         quantity: 1,
       },
     ],
-    mode: "payment",
+    billing_address_collection: "required",
+    mode: "subscription",
     ui_mode: "custom" as any,
     // The URL of your payment completion page
     return_url: "/foo",
