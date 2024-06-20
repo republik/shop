2b670a93bc48e45b4572b084abe435e9ba82ebc1
Tobias Maier <tobias.maier@republik.ch>
2024-06-20 14:16:21 +0200

    feat: show initials if no portrait is available

--- src/components/layout/portrait.tsx
+++ src/components/layout/portrait.tsx
@@ -1,16 +1,44 @@
 "use client";
 
 import { useQuery } from "@apollo/client";
-import { MeDocument } from "../../../graphql/republik-api/__generated__/gql/graphql";
+import {
+  MeDocument,
+  MeQuery,
+} from "../../../graphql/republik-api/__generated__/gql/graphql";
+import { Skeleton } from "../ui/skeleton";
 
 export function Portrait() {
-  const { data } = useQuery(MeDocument);
+  const { data, loading } = useQuery<MeQuery>(MeDocument);
   return (
     <div className="h-8 w-8">
-      {data?.me?.portrait ? (
-        <img src={data.me.portrait} alt="Portrait" className="h-8 w-8" />
+      {loading ? (
+        <Skeleton className="h-8 w-8" />
       ) : (
-        <div className="h-8 w-8 bg-gray-300"></div>
+        <>
+          {data?.me?.portrait ? (
+            <img src={data.me.portrait} alt="Portrait" className="h-8 w-8" />
+          ) : (
+            <div className="h-8 w-8 bg-gray-300/40">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="h-8 w-8"
+              >
+                <text
+                  x="50%"
+                  y="50%"
+                  dominantBaseline="middle"
+                  textAnchor="middle"
+                  fontSize={12}
+                  fontWeight={700}
+                >
+                  {data?.me?.initials || data?.me?.email?.[0] || "👻"}
+                </text>
+              </svg>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
