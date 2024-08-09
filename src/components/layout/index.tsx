7d960cbea0884987916243d0877cfa80a53433ed
Tobias Maier <tobias.maier@republik.ch>
2024-08-09 15:19:28 +0200

    feat: add footer links

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,6 +10,44 @@ import {
 } from "#graphql/republik-api/__generated__/gql/graphql";
 import { getClient } from "@/lib/graphql/client";
 
+function intersperse<T>(arr: T[], separator: T): T[] {
+  if (arr.length === 0) return [];
+
+  const result: T[] = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+    if (i < arr.length - 1) {
+      result.push(separator);
+    }
+  }
+
+  return result;
+}
+
+type Link = {
+  text: string;
+  href: string;
+};
+
+const footerLinks: Link[] = [
+  {
+    text: "Republik Magazin",
+    href: `${process.env.NEXT_PUBLIC_MAGAZIN_URL}`,
+  },
+  {
+    text: "Datenschutzbestimmung",
+    href: `${process.env.NEXT_PUBLIC_MAGAZIN_URL}/datenschutz`,
+  },
+  {
+    text: "Kontakt",
+    href: `${process.env.NEXT_PUBLIC_MAGAZIN_URL}/kontakt`,
+  },
+  {
+    text: "Hilfe",
+    href: `${process.env.NEXT_PUBLIC_MAGAZIN_URL}/faq`,
+  },
+];
+
 interface PageLayoutProps {
   children: React.ReactNode;
 }
@@ -29,6 +67,22 @@ export async function PageLayout({ children }: PageLayoutProps) {
     }
     return data;
   }
+
+  const footerLinkNodes = footerLinks.map((link) => (
+    <Link
+      key={link.href}
+      href={link.href}
+      className={css({
+        color: "[rgba(0, 0, 0, 0.50)]",
+        textAlign: "center",
+        _hover: {
+          textDecoration: "underline",
+        },
+      })}
+    >
+      {link.text}
+    </Link>
+  ));
 
   return (
     <div
@@ -82,8 +136,23 @@ export async function PageLayout({ children }: PageLayoutProps) {
       >
         {children}
       </main>
-      <footer className={container({ p: "4" })}>
-        <Link href="/impressum">{t("common:imprint")}</Link>
+      <footer
+        className={container({
+          py: "4",
+          px: "6",
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          columnGap: "2",
+          rowGap: "1",
+          justifyContent: "center",
+          color: "[rgba(0, 0, 0, 0.50)]",
+          borderTopColor: "[rgba(0, 0, 0, 0.2)]",
+          borderTopWidth: "1px",
+          borderTopStyle: "solid",
+        })}
+      >
+        {intersperse(footerLinkNodes, <span>-</span>)}
       </footer>
     </div>
   );
