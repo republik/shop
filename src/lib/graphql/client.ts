76a05ae15be661f9073e2e15c84bcd5740880c24
Tobias Maier <tobias.maier@republik.ch>
2024-08-09 12:09:54 +0200

    fix: add options param to getClient to allow configuring urql to set the received cookies to the res of the action/route-handler

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -2,7 +2,21 @@ import { Client, fetchExchange } from "@urql/core";
 import { cookies, headers } from "next/headers";
 import { parse } from "set-cookie-parser";
 
-export function getClient() {
+interface GetClientOptions {
+  // In case of actions or route-handlers, the received cookies can be set via nextjs' `cookies` accessor.
+  setReceivedCookies?: boolean;
+}
+
+/**
+ * Get a pre-configured urql-client instance.
+ * @param {GetClientOptions} options
+ * @returns {Client} urql-client
+ */
+export function getClient(
+  options: GetClientOptions = {
+    setReceivedCookies: false,
+  }
+): Client {
   const reqHeaders = {
     cookie: headers().get("cookie") ?? "",
     accept: headers().get("accept") ?? "",
@@ -21,9 +35,11 @@ export function getClient() {
     // Intercept fetch response from API to re-send cookies to the client
     async fetch(...args) {
       const res = await fetch(...args);
-      const setCookies = parse(res.headers.getSetCookie());
-      for (const cookie of setCookies) {
-        cookies().set(cookie);
+      if (options.setReceivedCookies) {
+        const setCookies = parse(res.headers.getSetCookie());
+        for (const cookie of setCookies) {
+          cookies().set(cookie);
+        }
       }
       return res;
     },
