c88beb390a9865110a03c17d9d11bd06df6eaf44
Olivier Baumann <olivierbaumann@me.com>
2024-09-17 11:24:29 +0200

    remove login and redirect home to angebote (#31)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,5 @@
-import { ProductCard } from "./components/product-card";
-import { SubscriptionsConfiguration } from "./angebot/[slug]/lib/config";
-import { container, grid } from "@/theme/patterns";
-import { fetchMe } from "@/lib/auth/fetch-me";
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
-  const me = await fetchMe();
-
-  return (
-    <div className={container({ gap: "16" })}>
-      <div
-        className={grid({
-          gap: "4",
-          gridTemplateColumns: "[1fr]",
-          md: {
-            gridTemplateColumns: "[repeat(auto-fit, minmax(24rem, 1fr))]",
-          },
-        })}
-      >
-        {Object.entries(SubscriptionsConfiguration).map(
-          ([subscriptionType, subscriptionConfiguration]) => (
-            <ProductCard
-              key={subscriptionType}
-              me={me}
-              subscriptionType={subscriptionType}
-              subscriptionConfiguration={subscriptionConfiguration}
-            />
-          )
-        )}
-      </div>
-    </div>
-  );
+  redirect(`${process.env.NEXT_PUBLIC_MAGAZIN_URL}/angebote`)
 }
