9212eb789f6e9e9a155ff7ea1c35108b19d62560
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-12-04 15:57:17 +0100

    increase max content width a bit

--- src/theme/preset.ts
+++ src/theme/preset.ts
@@ -145,7 +145,7 @@ export const presetRepublik = definePreset({
           },
         },
         sizes: {
-          maxContentWidth: { value: "52rem" },
+          maxContentWidth: { value: "54rem" },
           header: {
             height: { value: { base: "48px", md: "60px" } },
             avatar: { value: { base: "26px", md: "32px" } },
