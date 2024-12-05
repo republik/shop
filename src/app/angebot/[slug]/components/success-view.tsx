4d1187deb59c9867ce4cbb02cfcbaad0ead7f602
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-12-05 14:20:38 +0100

    show action on success

--- src/app/angebot/[slug]/components/success-view.tsx
+++ src/app/angebot/[slug]/components/success-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MeDocument } from "#graphql/republik-api/__generated__/gql/graphql";
-import { CheckoutSessionData } from "@/app/angebot/[slug]/lib/stripe/server";
+import type { CheckoutSessionData } from "@/app/angebot/[slug]/lib/stripe/server";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import useInterval from "@/lib/hooks/use-interval";
 import useTimeout from "@/lib/hooks/use-timeout";
@@ -8,6 +8,7 @@ import { css } from "@/theme/css";
 import { CheckCircleIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { usePlausible } from "next-plausible";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useQuery } from "urql";
 
@@ -16,11 +17,27 @@ type SuccessProps = {
   session: CheckoutSessionData;
 };
 
+const linkStyle = css({
+  borderRadius: "sm",
+  fontSize: "md",
+  whiteSpace: "nowrap",
+  px: "4",
+  py: "2",
+  display: "inline-block",
+  fontWeight: "medium",
+  textAlign: "center",
+  background: "text",
+  color: "text.inverted",
+});
+
 export function SubscriptionSuccess({ offer, session }: SuccessProps) {
+  const t = useTranslations("checkout.checkout.success.subscription");
+
   const [meRes, refetchMe] = useQuery({
     query: MeDocument,
   });
-  const t = useTranslations("checkout.checkout.success.subscription");
+
+  const ready = !!meRes.data?.me?.activeMagazineSubscription || true;
 
   const [
     startCheckingForActiveSubscription,
@@ -46,16 +63,16 @@ export function SubscriptionSuccess({ offer, session }: SuccessProps) {
     startCheckingForActiveSubscription ? 1_000 : null
   );
 
-  useEffect(() => {
-    if (
-      startCheckingForActiveSubscription &&
-      meRes.data?.me?.activeMagazineSubscription
-    ) {
-      window.location.assign(
-        `${process.env.NEXT_PUBLIC_MAGAZIN_URL}/einrichten?context=pledge&package=ABO`
-      );
-    }
-  }, [meRes.data, startCheckingForActiveSubscription]);
+  // useEffect(() => {
+  //   if (
+  //     startCheckingForActiveSubscription &&
+  //     meRes.data?.me?.activeMagazineSubscription
+  //   ) {
+  //     window.location.assign(
+  //       `${process.env.NEXT_PUBLIC_MAGAZIN_URL}/einrichten?context=pledge&package=ABO`
+  //     );
+  //   }
+  // }, [meRes.data, startCheckingForActiveSubscription]);
 
   return (
     <Alert variant="success">
@@ -66,7 +83,21 @@ export function SubscriptionSuccess({ offer, session }: SuccessProps) {
         })}
       />
       <AlertTitle>{t("title")}</AlertTitle>
-      <AlertDescription>{t("description")}</AlertDescription>
+      <AlertDescription>
+        {ready ? (
+          <>
+            <p className={css({ mb: "4" })}>{t("ready")}</p>
+            <Link
+              className={linkStyle}
+              href={`${process.env.NEXT_PUBLIC_MAGAZIN_URL}/einrichten?context=pledge&package=ABO`}
+            >
+              {t("action")}
+            </Link>
+          </>
+        ) : (
+          t("waiting")
+        )}
+      </AlertDescription>
     </Alert>
   );
 }
@@ -90,9 +121,17 @@ export function GiftSuccess({ offer, session }: SuccessProps) {
       />
       <AlertTitle>{t("title")}</AlertTitle>
       <AlertDescription>
-        {t.rich("description", {
-          email: (chunks) => <strong>{session.email}</strong>,
-        })}
+        <p className={css({ mb: "4" })}>
+          {t.rich("description", {
+            email: (chunks) => <strong>{session.email}</strong>,
+          })}
+        </p>
+        <Link
+          className={linkStyle}
+          href={`${process.env.NEXT_PUBLIC_MAGAZIN_URL}`}
+        >
+          {t("action")}
+        </Link>
       </AlertDescription>
     </Alert>
   );
