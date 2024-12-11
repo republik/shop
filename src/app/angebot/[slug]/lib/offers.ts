a8b549629b29a9794b281cf7e0206e1644c67f82
Jeremy Stucki <jeremy.stucki@gmail.com>
2024-12-11 16:24:55 +0100

    support promo codes via url param

--- src/app/angebot/[slug]/lib/offers.ts
+++ src/app/angebot/[slug]/lib/offers.ts
@@ -1,10 +1,10 @@
 import { GetOfferDocument } from "#graphql/republik-api/__generated__/gql/graphql";
 import { getClient } from "@/lib/graphql/client";
 
-export async function fetchOffer(offerId: string) {
+export async function fetchOffer(offerId: string, promoCode?: string) {
   const gql = getClient();
 
-  const { data } = await gql.query(GetOfferDocument, { offerId });
+  const { data } = await gql.query(GetOfferDocument, { offerId, promoCode });
 
   return data?.offer;
 }
