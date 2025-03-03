5616716a6fd37b2909ecb1f2ea49fdacf9080f0a
Jeremy Stucki <jeremy.stucki@gmail.com>
2025-03-03 17:54:28 +0100

    simplify availability check

--- src/lib/product-purchase-guards.ts
+++ src/lib/product-purchase-guards.ts
@@ -10,70 +10,6 @@ export type IsProductAvailableForUserPredicate = (
 ) => ProductAvailabilityResult;
 
 /**
- * Checks if the user can buy a monthly subscription based on their current
- * subscriptions and memberships.
- * @param me The user object
- * @returns An object with the availability status and a reason if not available
- */
-const canUserBuyMonthlyAbo: IsProductAvailableForUserPredicate = (me) => {
-  // Subscriptions
-  if (me.activeMagazineSubscription) {
-    return {
-      available: false,
-      reason: "hasSubscription",
-    };
-  }
-
-  // Legacy memberships
-  // Check if membership has ended, i.e. membership is in grace period. In this case we allow buying a new subscription.
-  const legacyMembershipHasEnded =
-    me.activeMembership?.endDate &&
-    new Date(me.activeMembership?.endDate).getTime() < Date.now();
-
-  if (me.activeMembership && !legacyMembershipHasEnded) {
-    return {
-      available: false,
-      reason: "hasSubscription",
-    };
-  }
-
-  // Go!
-  return { available: true };
-};
-
-/**
- * Checks if the user can buy a yearly subscription based on their current
- * subscriptions and memberships.
- * @param me The user object
- * @returns An object with the availability status and a reason if not available
- */
-const canUserBuyYearlyAbo: IsProductAvailableForUserPredicate = (me) => {
-  // Subscriptions
-  if (me.activeMagazineSubscription) {
-    return {
-      available: false,
-      reason: "hasSubscription",
-    };
-  }
-
-  // Legacy memberships
-  // Check if membership has ended, i.e. membership is in grace period. In this case we allow buying a new subscription.
-  const legacyMembershipHasEnded =
-    me.activeMembership?.endDate &&
-    new Date(me.activeMembership?.endDate).getTime() < Date.now();
-
-  if (me.activeMembership && !legacyMembershipHasEnded) {
-    return {
-      available: false,
-      reason: "hasSubscription",
-    };
-  }
-
-  // Go!
-  return { available: true };
-};
-
-/**
  * Checks if the user can buy a subscription based on their current
  * subscriptions and memberships.
  * @param me The user object
@@ -84,12 +20,27 @@ export function checkIfUserCanPurchase(
   me: Me,
   offerId: string
 ): ReturnType<IsProductAvailableForUserPredicate> {
-  if (offerId === "MONTHLY") {
-    return canUserBuyMonthlyAbo(me);
-  }
-  if (offerId === "YEARLY") {
-    return canUserBuyYearlyAbo(me);
+  // Subscriptions
+  if (me.activeMagazineSubscription) {
+    return {
+      available: false,
+      reason: "hasSubscription",
+    };
   }
 
-  return { available: false };
+  // Legacy memberships
+  // Check if membership has ended, i.e. membership is in grace period. In this case we allow buying a new subscription.
+  const legacyMembershipHasEnded =
+    me.activeMembership?.endDate &&
+    new Date(me.activeMembership?.endDate).getTime() < Date.now();
+
+  if (me.activeMembership && !legacyMembershipHasEnded) {
+    return {
+      available: false,
+      reason: "hasSubscription",
+    };
+  }
+
+  // Go!
+  return { available: true };
 }
