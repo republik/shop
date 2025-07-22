2d5ab34ad3eeee8872947dc77f30c6ff3f0fcb04
Jeremy Stucki <jeremy.stucki@gmail.com>
2025-07-22 14:13:48 +0200

    refactor: simplify landing page code

--- src/app/u30/page.tsx
+++ src/app/u30/page.tsx
@@ -1,11 +1,15 @@
-import { css } from "@/theme/css";
-import { visuallyHidden } from "@/theme/patterns";
-import { getTranslations } from "next-intl/server";
-
+import giftBigSrc from "@/assets/gift-big.svg";
+import giftSmallSrc from "@/assets/gift-small.svg";
 import { Hero } from "@/components/layout/hero";
+import { DescriptionItem } from "@/components/overview/description-item";
 import { U30Chooser } from "@/components/u30/u30-chooser";
 import { LandingPageLayout } from "@/layouts/landing-page";
 import { readAnalyticsParamsFromCookie } from "@/lib/analytics";
+import { css } from "@/theme/css";
+import { visuallyHidden } from "@/theme/patterns";
+import { PiggyBankIcon } from "lucide-react";
+import { getTranslations } from "next-intl/server";
+import Image from "next/image";
 
 export async function generateMetadata() {
   const t = await getTranslations("landing.u30");
@@ -15,8 +19,34 @@ export async function generateMetadata() {
   };
 }
 
-export default async function GiftsPage() {
+const styles = {
+  yearlyOnly: css({
+    display: "block",
+    ":has([value='MONTHLY']:checked) &": {
+      display: "none",
+    },
+  }),
+  monthlyOnly: css({
+    display: "none",
+    ":has([value='MONTHLY']:checked) &": {
+      display: "block",
+    },
+  }),
+};
+
+export default async function U30LandingPage() {
   const t = await getTranslations("landing.u30");
+  const tDescriptionItems = await getTranslations(
+    "landing.u30.description.items"
+  );
+
+  const getText = (
+    tKey: "dialog" | "general" | "briefings" | "disclaimer" | "allTheContent"
+  ) =>
+    tDescriptionItems.rich(tKey, {
+      b: (chunks) => <b>{chunks}</b>,
+      p: (chunks) => <p className={css({ mt: "2" })}>{chunks}</p>,
+    });
 
   // TODO remove this again when we don't redirect to legacy /angebote
   const analyticsParams = await readAnalyticsParamsFromCookie();
@@ -39,7 +69,39 @@ export default async function GiftsPage() {
         </p>
       </Hero>
 
+      <Image
+        className={styles.yearlyOnly}
+        src={giftBigSrc}
+        width={327}
+        height={200}
+        alt="Illustration grosses Paket"
+      />
+      <Image
+        className={styles.monthlyOnly}
+        src={giftSmallSrc}
+        width={327}
+        height={200}
+        alt="Illustration kleines Paket"
+      />
+
       <U30Chooser analyticsParams={analyticsParams} />
+
+      <ul
+        className={css({
+          display: "flex",
+          flexDirection: "column",
+          gap: "2",
+          fontSize: "lg",
+        })}
+      >
+        <DescriptionItem icon={<PiggyBankIcon />}>
+          {getText("disclaimer")}
+        </DescriptionItem>
+        <DescriptionItem>{getText("general")}</DescriptionItem>
+        <DescriptionItem>{getText("allTheContent")}</DescriptionItem>
+        <DescriptionItem>{getText("briefings")}</DescriptionItem>
+        <DescriptionItem>{getText("dialog")}</DescriptionItem>
+      </ul>
     </LandingPageLayout>
   );
 }
--- src/components/gifts/gift-chooser.tsx
+++ src/components/gifts/gift-chooser.tsx
@@ -1,20 +1,10 @@
 "use client";
 
-import giftBigSrc from "@/assets/gift-big.svg";
-import giftSmallSrc from "@/assets/gift-small.svg";
-import { GiftDescription } from "@/components/gifts/gift-description";
-import { cardButton } from "@/components/ui/card-button";
+import { Button } from "@/components/ui/button";
+import { LandingPageOption } from "@/components/ui/landing-page-radio-option";
 import type { AnalyticsObject } from "@/lib/analytics";
 import { css } from "@/theme/css";
-import { token } from "@/theme/tokens";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
-import {
-  type ChangeEventHandler,
-  type ReactNode,
-  useId,
-  useState,
-} from "react";
 
 export function GiftChooser({
   analyticsParams,
@@ -23,160 +13,43 @@ export function GiftChooser({
   analyticsParams?: AnalyticsObject;
 }) {
   const t = useTranslations("landing.gifts");
-  const [option, setOption] = useState<string>("ABO_GIVE");
-
-  const handleOption: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setOption(e.currentTarget.value);
-  };
 
   return (
-    <>
-      <Image
-        src={giftBigSrc}
-        width={327}
-        height={200}
-        hidden={option === "ABO_GIVE_MONTHS"}
-        alt="Illustration grosses Paket"
-      />
-      <Image
-        src={giftSmallSrc}
-        width={327}
-        height={200}
-        hidden={option === "ABO_GIVE"}
-        alt="Illustration kleines Paket"
-      />
-
-      <form
-        action={`${process.env.NEXT_PUBLIC_MAGAZIN_URL}/angebote`}
-        method="GET"
+    <form
+      action={`${process.env.NEXT_PUBLIC_MAGAZIN_URL}/angebote`}
+      method="GET"
+      className={css({
+        display: "flex",
+        flexDirection: "column",
+        gap: "8",
+        width: "full",
+      })}
+    >
+      {
+        // TODO remove this again when we don't redirect to legacy /angebote
+        analyticsParams
+          ? Object.entries(analyticsParams).map(([k, v]) => {
+              return <input key={k} name={k} value={v} hidden readOnly />;
+            })
+          : null
+      }
+      <div
         className={css({
           display: "flex",
           flexDirection: "column",
-          gap: "8",
-          width: "full",
+          gap: "2",
         })}
       >
-        {
-          // TODO remove this again when we don't redirect to legacy /angebote
-          analyticsParams
-            ? Object.entries(analyticsParams).map(([k, v]) => {
-                return <input key={k} name={k} value={v} hidden readOnly />;
-              })
-            : null
-        }
-        <div
-          className={css({
-            display: "flex",
-            flexDirection: "column",
-            gap: "2",
-          })}
-        >
-          <Option
-            value="ABO_GIVE"
-            selected={option === "ABO_GIVE"}
-            onChange={handleOption}
-          >
-            <strong>{t("options.yearly")}</strong> CHF 222
-          </Option>
-          <Option
-            value="ABO_GIVE_MONTHS"
-            selected={option === "ABO_GIVE_MONTHS"}
-            onChange={handleOption}
-          >
-            <strong>{t("options.monthly")}</strong> CHF 48
-          </Option>
-        </div>
+        <LandingPageOption name="package" value="ABO_GIVE" defaultChecked>
+          <strong>{t("options.yearly")}</strong> CHF 222
+        </LandingPageOption>
 
-        <button
-          type="submit"
-          className={cardButton({ visual: "solid" })}
-          style={{
-            // @ts-expect-error custom css vars
-            "--text": token("colors.text"),
-            "--cta": token("colors.white"),
-          }}
-        >
-          {t("cta")}
-        </button>
-      </form>
+        <LandingPageOption name="package" value="ABO_GIVE_MONTHS">
+          <strong>{t("options.monthly")}</strong> CHF 48
+        </LandingPageOption>
+      </div>
 
-      <GiftDescription
-        interval={option === "ABO_GIVE_MONTHS" ? "monthly" : "yearly"}
-      />
-    </>
-  );
-}
-
-function Option({
-  value,
-  selected,
-  children,
-  onChange,
-}: {
-  value: string;
-  selected: boolean;
-  children: ReactNode;
-  onChange: ChangeEventHandler<HTMLInputElement>;
-}) {
-  const id = useId();
-  return (
-    <label
-      className={css({
-        // borderWidth: 2,
-        // borderStyle: "solid",
-        // borderRadius: "5px",
-        // borderColor: "disabled",
-        w: "full",
-        display: "flex",
-        gap: "4",
-        alignItems: "center",
-        "&:has(:checked)": {
-          borderColor: "text",
-        },
-        fontSize: "xl",
-      })}
-    >
-      <input
-        id={id}
-        value={value}
-        name="package"
-        type="radio"
-        checked={selected}
-        onChange={onChange}
-        className={css({
-          flexShrink: 0,
-          // Custom checkbox style, see https://moderncss.dev/pure-css-custom-styled-radio-buttons/
-          appearance: "none",
-          backgroundColor: "transparent",
-          margin: "0",
-          color: "current",
-          width: "[1.15em]",
-          height: "[1.15em]",
-          borderWidth: 2,
-          borderStyle: "solid",
-          borderColor: "text",
-          borderRadius: "full",
-          display: "grid",
-          placeContent: "center",
-          outline: "none",
-          _before: {
-            content: '""',
-            width: "[0.35em]",
-            height: "[0.35em]",
-            borderRadius: "full",
-            backgroundColor: "transparent",
-          },
-
-          _checked: {
-            backgroundColor: "text",
-            _before: {
-              backgroundColor: "text.inverted",
-            },
-          },
-        })}
-      />
-
-      <span>{children}</span>
-    </label>
+      <Button type="submit">{t("cta")}</Button>
+    </form>
   );
 }
